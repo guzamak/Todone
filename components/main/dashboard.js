257588764ddea8b6230c602064a521c921272720
guzamak <120812215+guzamak@users.noreply.github.com>
2024-04-21 04:44:27 +0700

    Update dashboard.js

--- components/main/dashboard.js
+++ components/main/dashboard.js
@@ -1,6 +1,6 @@
 "use client";
 import Login from "./login";
 import Todo from "./todo";
-export default function dashboard({ session }) {
+export default function Dashboard({ session }) {
   return <>{session?.user ? <Todo /> : <Login />}</>;
 }
